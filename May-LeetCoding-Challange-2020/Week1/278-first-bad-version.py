210a96483d067bf55e6ea9bbf0eae407e141c957
Sangwon Song <mrsong0822@gmail.com>
2020-05-01 16:17:26 -0700

    189, 278 solved

--- May-LeetCoding-Challange-2020/Week1/278-first-bad-version.py
+++ May-LeetCoding-Challange-2020/Week1/278-first-bad-version.py
@@ -4,9 +4,37 @@
 # def isBadVersion(version):
 
 class Solution:
+    # Sol 1. Linear Search
     def firstBadVersion(self, n):
         """
         :type n: int
         :rtype: int
         """
+        for i in range(n):
+            if(isBadVersion(i)):
+                return i
+        return n
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
+        # Working, Not accepted, time-limit exceeded
+
+    # Sol 2. Binary Search Tree
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left = 1 
+        right = n
         
+        while(left<right):
+            mid = left+(right-left)//2 # In Python 3, / returns floating-point and // returns integer 
+            
+            if not isBadVersion(mid):
+                left = mid + 1
+            else: 
+                right = mid
+        
+        return left
+        # Time Complexity: O(log(n))
+        # Space Complexity: O(1)
--- Top-Interview-Questions/Array/Easy-Collection/189-rotate-array.py
+++ Top-Interview-Questions/Array/Easy-Collection/189-rotate-array.py
@@ -6,7 +6,6 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-     
         for i in range(k):
             prev = nums[-1]
             for j in range(len(nums)):
@@ -14,4 +13,20 @@ class Solution(object):
     # Time Complexity: O(k x n) -> O(n)
     # Space Complexity: O(1)
     # Working, but not accepted, time limite exceeded issue
+    # Reason: Edge case of the list size, 536,870,91. If the list has the same
+    # size of the max-size, it cannot exchange the element, head and tail.
+    # 
+
+    # Sol 2. Brute Force 2
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        
+        nums[:k], nums[k:] = nums[len(nums)-k:], nums[:len(nums)-k]
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)     
 
