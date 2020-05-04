6d43ff413111c8b9326e5a07c463a89f6b70f29b
Sangwon Song <mrsong0822@gmail.com>
2020-05-03 21:45:33 -0700

    383 ransom note.py

---
+++ May-LeetCoding-Challange-2020/Week1/383-ransom-note.py
@@ -0,0 +1,8 @@
+class Solution:
+    # Sol 1. using Counter, Brute force
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ran_col = collections.Counter(ransomNote)
+        mag_col = collections.Counter(magazine)
+        return ran_col&mag_col==ran_col
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
