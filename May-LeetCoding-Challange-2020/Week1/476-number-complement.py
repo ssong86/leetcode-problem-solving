17e7ade255c62850f12390dc89e4203b920b13c6
Sangwon Song <mrsong0822@gmail.com>
2020-05-04 21:40:40 -0700

    476 number complement.py

---
+++ May-LeetCoding-Challange-2020/Week1/476-number-complement.py
@@ -0,0 +1,24 @@
+class Solution:
+    # Sol 1. XOR Bit Manipulation
+    def findComplement(self, num: int) -> int:
+        # Base case
+        if num == 0:
+            return 1
+        
+        # ex) 5 = 101, 1 = 001
+        # 101 XOR 001 = 100
+        # 100 XOR 010 = 110
+        # 110 XOR 100 = 010
+        
+        loop_controller = num
+        bit_mask = 1
+        while loop_controller:
+            # flip current bit
+            # XOR
+            num = num ^ bit_mask
+
+            bit_mask = bit_mask << 1
+            loop_controller = loop_controller >> 1
+        return num
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
