0a3e863b4c40c8118f1e044637545149f363695b
Sangwon Song <mrsong0822@gmail.com>
2020-05-08 21:04:23 -0700

    1232 completed py

---
+++ May-LeetCoding-Challange-2020/Week2/1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,15 @@
+class Solution:
+    # Sol 1. Brute Force Math
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        # print(coordinates[0])
+        # delta = (yn-y0)/(xn-x0), fuck this
+        x0,y0 = coordinates[0]
+        x1,y1 = coordinates[1]
+        for i in range(2,len(coordinates)):
+            xn, yn = coordinates[i]
+            if (y1-y0)*(xn-x0) == (yn-y0)*(x1-x0):
+                return True
+            else:
+                return False
+        # Time Complexity: O(n)
+        # Space Complexity: O(1)
