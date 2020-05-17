a8abbd5cc2ac4ba468101f7fb17c19bcb48c587f
Sangwon Song <mrsong0822@gmail.com>
2020-05-16 20:56:45 -0700

    328 more solutions py

--- May-LeetCoding-Challange-2020/Week3/328-odd-even-linked-list.py
+++ May-LeetCoding-Challange-2020/Week3/328-odd-even-linked-list.py
@@ -30,3 +30,51 @@ class Solution:
     # Submit Runtime: 68 ms
     # Submit Memory: 15.7 MB
     # Submit Runtime Distribution: 7.57% faster, fuck this 
+
+    # Sol 2. 
+    class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        even=ListNode(0)
+        et=even
+        odd=ListNode(0)
+        ot=odd
+        count=1
+        
+        while head!=None:
+            if count%2==1:
+                ot.next=head
+                ot=ot.next
+                
+            else:
+                et.next=head
+                et=et.next
+                
+            head=head.next
+            count+=1
+            
+        et.next=None
+        ot.next=even.next
+        
+        return odd.next
+
+    # Sol 3. Most optimized
+    class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        cur = head
+        values = []
+        while cur:
+            values.append(cur.val)
+            cur = cur.next
+            
+        i = 0
+        cur = head
+        while i < len(values):
+            cur.val = values[i]
+            cur = cur.next
+            i+=2
+        i = 1
+        while i < len(values):
+            cur.val = values[i]
+            cur = cur.next
+            i+=2
+        return head
