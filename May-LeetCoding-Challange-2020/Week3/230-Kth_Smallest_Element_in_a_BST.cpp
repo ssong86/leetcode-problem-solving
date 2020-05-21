85c4147780cdd9877c83e42724d822d5d4c43a65
gyuho945 <gyuho945@gmail.com>
2020-05-20 22:28:10 -0700

    rotate image and day 20

---
+++ May-LeetCoding-Challange-2020/Week3/230-Kth_Smallest_Element_in_a_BST.cpp
@@ -0,0 +1,52 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// Time Complexity: O(logn)
+// Space Complexity: O(1)
+// Runtime: 24 ms, faster than 66.61%
+// Memory Usage: 24 MB, less than 6.67%
+
+// Best case: kth on the left most tree O(height of tree)
+// Worst case: kth on the right most tree O(logn) in which n is size of the tree
+
+
+class Solution {
+public:
+    
+    int result, counter;
+    
+    void getValue(TreeNode *root){
+        
+        if(root == NULL)
+            return;
+        
+        getValue(root->left);
+        
+        counter--;
+        if(counter == 0){
+            result = root->val;
+            return;
+        }
+        
+        getValue(root->right);
+        
+    }
+    
+    int kthSmallest(TreeNode* root, int k) {
+        
+        counter = k;
+        getValue(root);
+        
+        return result;
+    }
+};
