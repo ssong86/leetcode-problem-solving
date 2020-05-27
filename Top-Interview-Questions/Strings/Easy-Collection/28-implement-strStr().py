67b7c0e7547126dfd76759cf3de023e26b4320ff
Sangwon Song <mrsong0822@gmail.com>
2020-05-26 18:33:02 -0700

    28 soloved py

---
+++ Top-Interview-Questions/Strings/Easy-Collection/28-implement-strStr().py
@@ -0,0 +1,27 @@
+class Solution:
+    # Sol 1. Using Find built-in function
+    def strStr(self, haystack: str, needle: str) -> int:
+        
+        return haystack.find(needle)
+        # find returns -1 if no such string
+        # index returns error if searching is not present
+    # TC: O(n)
+    # SC: O(1)
+    # Runtime: 32ms, 58.97% faster
+    # Memory: 14.1 MB, 12.31% less
+
+    # Sol 2. Sliding Windows
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0 # corner case
+        for i in range(len(haystack)): # traverse each index, len(heystack) - len(needle) + 1 will be faster
+                                       # no need to look up the element after the haystack 
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+    # TC: O(n), where n is the length of haystack, worst case is that
+    # we have to go thru all the element in haystack and needle length is one, and 
+    # the index is located in the last poition of haystack
+    # SC: O(1), no additional memories
+    # Runtime: 36 ms, faster than 41.06%
+    # 13.9 MB, less than 12.31%
