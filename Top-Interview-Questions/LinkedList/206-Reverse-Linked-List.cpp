60dfd723bf2367d1894e6eb78ff5229ff983ae40
gyuho945 <gyuho945@gmail.com>
2020-06-05 01:17:47 -0700

    addewd iterative and recursive solution

--- Top-Interview-Questions/LinkedList/206-Reverse-Linked-List.cpp
+++ Top-Interview-Questions/LinkedList/206-Reverse-Linked-List.cpp
@@ -50,3 +50,75 @@ public:
         return head;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// Time Complexity: O(n)
+// Space Complexiy: O(1)
+// Runtime: 4 ms, faster than 96.70%
+// Memory Usage: 7.8 MB, less than 53.80%
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        
+        ListNode *prev, *current;
+        prev = NULL;
+        current = head;
+        
+        while(current != NULL){
+            ListNode *next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        
+        return prev;
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// Time Complexity: O(n)
+// Space Complexiy: O(n)
+// Runtime: 8 ms, faster than 66.93%
+// Memory Usage: 8.1 MB, less than 18.68% 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        
+        ListNode *ret = reverseList(head->next);
+        
+        head->next->next = head;
+        head->next = NULL;
+        
+        return ret;
+        
+    }
+};
