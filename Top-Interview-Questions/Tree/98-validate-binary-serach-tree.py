edd62216d4d216758979f9c6a30f1c79b6da371d
Sangwon Song <mrsong0822@gmail.com>
2020-06-09 19:58:26 -0700

    6/9 top interview solved py

---
+++ Top-Interview-Questions/Tree/98-validate-binary-serach-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        stack, res = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            # if root is None or all the nodes have 
+            # been traversed and have no confliction 
+            if not stack:
+                return True
+            node = stack.pop()
+            # res stores the current values in in-order 
+            # traversal order, node.val should larger than
+            # the last element in res
+            if res and node.val <= res[-1]:
+                return False
+            res.append(node.val)
+            root = node.right
+    # TC: O(n)
+    # SC: O(n), worst, O(logn), best
