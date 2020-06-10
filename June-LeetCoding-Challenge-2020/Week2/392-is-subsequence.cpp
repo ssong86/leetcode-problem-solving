4832853aa79ef6e862d0d7b71d98799bfe070e18
gyuho945 <gyuho945@gmail.com>
2020-06-09 20:24:35 -0700

    space complexity of O(1) solution added

--- June-LeetCoding-Challenge-2020/Week2/392-is-subsequence.cpp
+++ June-LeetCoding-Challenge-2020/Week2/392-is-subsequence.cpp
@@ -5,6 +5,7 @@
 class Solution {
 public:
 
+ //s = "abc", t = "ahbgdc"
     bool isSubsequence(string s, string t) {
         
         if(s.length() > t.length())
@@ -21,7 +22,6 @@ public:
         for(int i = 0; i < s.length(); i++){
             q.push(s[i]);
         }
-
         
         for(int i = 0; i < t.length(); i++){
             if(t[i] == q.front()){
@@ -33,3 +33,32 @@ public:
     }
 };
 
+// Time Complexity: O(t)
+// Space Complexity: O(1)
+// Runtime: 4 ms, faster than 84.88%
+// Memory Usage: 6.3 MB, less than 95.10%
+class Solution {
+public:
+
+    bool isSubsequence(string s, string t) {
+        
+        if(s.length() > t.length())
+            return false;
+        
+        if(!s.length())
+            return true;
+        
+        if(!t.length())
+            return false;
+        
+        int pointer = 0;
+
+        for(char comp : t){
+            if(s[pointer] == comp){
+                pointer++;
+            }
+        }
+        
+        return pointer == s.length();
+    }
+};
