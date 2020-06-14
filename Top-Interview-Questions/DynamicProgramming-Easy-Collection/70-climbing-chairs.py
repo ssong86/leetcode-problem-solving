1c4ee2fc451b55cb0b568545304bd800fca6edf2
Sangwon Song <mrsong0822@gmail.com>
2020-06-13 22:21:32 -0700

    6/13 solved  py and folder arrange

---
+++ Top-Interview-Questions/DynamicProgramming-Easy-Collection/70-climbing-chairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    # Sol 1. DP
+    def climbStairs(self, n: int) -> int:
+        # we only can use 1 or 2 step
+        if n == 1:
+            return n
+        
+        dp = [0] * (n+1) # dp = [0] * n
+        dp[1] = 1 # dp[0] = 1
+        dp[2] = 2 # dp[1] = 2
+        for i in range(3, n+1): # for i in range(2, n)
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n] # return dp[n-1]
+    # TC: O(n)
+    # SC: O(n)
+    # Runtime: 28 ms, faster than 74.05%
