12610f9c4731e52760eda34a9400ddf44f99f3a5
gyuho945 <gyuho945@gmail.com>
2020-06-21 21:04:42 -0700

    shuffle an array added sol #2

--- Top-Interview-Questions/Design/384-Shuffle-an-Array.cpp
+++ Top-Interview-Questions/Design/384-Shuffle-an-Array.cpp
@@ -1,3 +1,4 @@
+// Solution #1 Brute Force
 // TC: O(n^2) in shuffled because of erase 
 // SC: O(n) # of elements in nums
 // Runtime: 184 ms, faster than 44.06%
@@ -33,6 +34,55 @@ public:
         
         return shuffled;
         
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
+
+// Solution #2 Fisher-Yates Algorithm
+// TC: O(n) in shuffled 
+// SC: O(n) # of elements in nums
+// Runtime: 160 ms, faster than 92.20%
+// Memory Usage: 90.6 MB, less than 22.61%
+class Solution {
+public:
+    vector<int> array, shuffled;
+    
+    void swap(vector<int> &array, int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    
+    int random_index(int start, int end){
+        return rand()%(end-start)+start;
+    }
+    
+    Solution(vector<int>& nums) {
+        array = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return array;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        int n = shuffled.size();
+        
+        shuffled = array;
+        
+        for(int i = 0; i < n; i++){
+            swap(shuffled, i, random_index(i,n));
+        }
+        
+        return shuffled;
     }
 };
 
