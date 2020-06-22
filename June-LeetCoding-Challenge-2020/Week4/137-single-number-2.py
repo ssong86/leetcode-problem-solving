1be3f461ab4c7c10a110410ff7dda9aab8249b06
Sangwon Song <mrsong0822@gmail.com>
2020-06-22 00:35:10 -0700

    .

--- June-LeetCoding-Challenge-2020/Week4/137-single-number-2.py
+++ June-LeetCoding-Challenge-2020/Week4/137-single-number-2.py
@@ -2,6 +2,6 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         return 1
     # TC: O(n)
-    # SC: O(1)
+    # SC: O(1)...
     # Runtime: 88 ms, faster than 71.22% 
     # Memory Usage: 16.4 MB, less than 37.73%
