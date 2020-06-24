e0f8ff5a39e6eab140262f04eb2088abf1fcbee3
Sangwon Song <sangwon.song@sjsu.edu>
2020-06-24 10:56:29 -0700

    96 sol 2 TLE py sangwon

--- June-LeetCoding-Challenge-2020/Week4/96-unique-binary-search-tree.py
+++ June-LeetCoding-Challenge-2020/Week4/96-unique-binary-search-tree.py
@@ -6,7 +6,7 @@ class Solution:
         elif n is 1:
             return 1
         return self.catalan_number(n)
-    
+ 
     def catalan_number(self, c:int) -> int:
         if c<=1:
             return 1
@@ -18,3 +18,21 @@ class Solution:
     # SC: O(1)
     # Runtime: 32 ms, faster than 47.87% 
     # Memory Usage: 13.9 MB, less than 38.13% 
+
+    # Sol 2. divide and conquer
+    def numTrees(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        def d_c(start,end):
+            if start<1 or end>n or start>=end:
+                return 1
+            
+            count = 0
+            for i in range(start, end+1):
+                count = count + d_c(start, i-1)*d_c(i+1,end)
+                
+            return count
+        
+        return d_c(1,n)
