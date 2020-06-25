4951b03e2604dca0e586e0b5c7f438fc855420bf
Sangwon Song <mrsong0822@gmail.com>
2020-06-24 20:50:18 -0700

    6/20 96 sol2 solved py sangwon

--- June-LeetCoding-Challenge-2020/Week4/96-unique-binary-search-tree.py
+++ June-LeetCoding-Challenge-2020/Week4/96-unique-binary-search-tree.py
@@ -24,15 +24,23 @@ class Solution:
         if n == 0:
             return 0
         elif n == 1:
-            return 1
+            return 1  
+        mem = {} # memorization table
         def d_c(start,end):
             if start<1 or end>n or start>=end:
                 return 1
-            
             count = 0
-            for i in range(start, end+1):
-                count = count + d_c(start, i-1)*d_c(i+1,end)
-                
+            if (start, end) in mem:
+                return mem[(start, end)]
+            else:
+                for i in range(start, end+1):
+                    count += d_c(start, i-1)*d_c(i+1, end)
+                mem[(start,end)] = count
             return count
         
         return d_c(1,n)
+    # TC: O(n*h), n is number of nodes, h is depth of each tree, worst case n^2
+    # SC: O(n), map m is declared in the scope
+    # Runtime: 28 ms, faster than 76.04%
+    # Memory Usage: 13.8 MB, less than 50.56%  
+    
