e3a3a06f4ab4e997707d0697d906fcfb7c7e9f20
moh008 <moh008@ucr.edu>
2020-06-24 21:24:44 -0700

    Top-Interview-Questions Fizzbuzz

---
+++ Top-Interview-Questions/Math-Easy-Collection/412-Fizz-Buzz-moh.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> result;
+        string str1;
+        for (int i = 1; i <= n; i++){
+            if(i % 15 == 0)
+                result.push_back("FizzBuzz");
+            else if(i % 5 == 0)
+                result.push_back("Buzz");
+            else if(i % 3 == 0)
+                result.push_back("Fizz");
+            else{
+                str1 = to_string(i);
+                result.push_back(str1);
+            }
+        }
+        return result;
+    }
+};
