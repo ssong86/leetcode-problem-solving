56fc5a4076adaee53f5c23a2691e9ddc06f25bfc
Sangwon Song <mrsong0822@gmail.com>
2020-07-02 20:06:07 -0700

    fixed overlapps

--- July-LeetCoding-Challenge-2020/Week1/107-binary-tree-level-order-traversal-ii.py
+++ July-LeetCoding-Challenge-2020/Week1/107-binary-tree-level-order-traversal-ii.py
@@ -7,24 +7,21 @@
 class Solution:
     # Sol 1. DFS    
     def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
-        levels = []
-        next_level = deque([root])
+        result=[]
+        if not root:
+            return result
+        self.dfs(root,0,result)
         
-        while root and next_level:
-            curr_level = next_level
-            next_level = deque()
-            levels.append([])
-            
-            for node in curr_level:
-                # append the current node value
-                levels[-1].append(node.val)
-                # process child nodes for the next level
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-                    
-        return levels[::-1]
+        return result
+    
+    def dfs(self,root,level,res):
+        if not root:
+            return 
+        if len(res) < level+1: # current level
+            res.append([])
+        self.dfs(root.left, level+1, res)
+        self.dfs(root.right, level+1, res)
+        res[level].append(root.val)
     # TC: O(N)
     # SC: O(N)
 
