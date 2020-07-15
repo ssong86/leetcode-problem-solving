7a87a08a130c620e5f07ef966a102fbe29ffb7ee
Sangwon Song <sangwon.song@sjsu.edu>
2020-07-15 16:31:25 -0700

    week 3 added and 7/15 solved py sangwon

--- July-LeetCoding-Challenge-2020/Week3/151-reverse-words-in-a-string.py
+++ July-LeetCoding-Challenge-2020/Week3/151-reverse-words-in-a-string.py
@@ -2,5 +2,5 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(reversed(s.split()))
 
-    # TC: O(1)
+    # TC: O(n), split runs n words in the string
     # SC: O(n), split provides n words space
