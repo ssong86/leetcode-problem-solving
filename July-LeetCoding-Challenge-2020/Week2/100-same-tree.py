a87847f4977a4d40538b906d17403d152cd6281d
Sangwon Song <mrsong0822@gmail.com>
2020-07-15 23:10:33 -0700

    cleaning branch

--- July-LeetCoding-Challenge-2020/Week2/100-same-tree.py
+++ July-LeetCoding-Challenge-2020/Week2/100-same-tree.py
@@ -17,5 +17,7 @@ class Solution:
             return False
         
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-    # TC: O(N)
-    # SC: O(logN)
+    # TC: O(N), minimum number of nodes in p or q
+    # SC: O(h), where h is the shorter depth in p or q, h represents the height of a tree
+    # Runtime: 28 ms, faster than 81.78%
+    # Memory Usage: 13.9 MB, less than 24.53%
